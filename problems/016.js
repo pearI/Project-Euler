aa4c015894f3a9fe4be6ce4b528f5b4193e0ffd0
Yannick Thomassen <50179823+pearI@users.noreply.github.com>
2019-05-02 17:22:47 +0200

    Problem 016: Added solution

---
+++ problems/016.js
@@ -0,0 +1,18 @@
+/*
+ * Power digit sum
+ * 
+ * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * What is the sum of the digits of the number 2^1000?
+ */
+
+const BigNumber = require( 'big-number' );
+
+// Evaluate 2 to the power of 1000 and iterate through every character.
+// After summing up all the characters we are left with out answer.
+let sum = 0;
+const number = BigNumber( 2 ).pow( 1000 ).toString();
+for ( let i = 0; i < number.length; i++ ) {
+    sum += parseInt( number[ i ] );
+}
+
+console.log( sum );
