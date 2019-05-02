e519287214a3ec6e48c44f639f34e7d837f60475
Yannick Thomassen <50179823+pearI@users.noreply.github.com>
2019-05-02 11:25:40 +0200

    Problem 004: Corrected solution

--- problems/004.js
+++ problems/004.js
@@ -11,12 +11,12 @@
 // 3-digit numbers. Doing this in reverse assures us that the first palindromic
 // number is also the largest. Each iteration we check if the evaluated number
 // as string equals that string in reverse, when it does, we have our answer.
-loop: for ( let i = 1000; i > 100; i-- ) {
+const palindromes = [];
+for ( let i = 1000; i > 100; i-- ) {
     for ( let j = 1000; j > 100; j-- ) {
         const number = `${ i * j }`, reversed = number.split( "" ).reverse().join( "" );
-        if ( number === reversed ) {
-            console.log( number );
-            break loop;
-        }
+        if ( number === reversed ) palindromes.push( number );
     }
 }
+
+console.log( Math.max( ...palindromes ) );
